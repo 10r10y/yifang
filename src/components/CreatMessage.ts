7bbf3c002eb2261ef370e6ba08031081a5b99d5d
dyc <1030900572@qq.com>
2022-08-11 23:53:55 +0800

    0

--- src/components/CreatMessage.ts
+++ src/components/CreatMessage.ts
@@ -5,25 +5,26 @@ export type MessageType= 'success' | 'error' | 'default';
 
 // 函数式创建组件
 const createMessage = (message: string, type: MessageType, timeout?: number) => {
-    // 使用 h 函数创建 Vnode 节点
+    // 1. 使用 h 函数创建 Vnode 节点
     const messageVnode = h(Message, {
         message,
         type
     })
+    // 创建一个根节点用于挂载 Vnode
     const mountNode = document.createElement('div');
     document.body.appendChild(mountNode);
 
-    // 使用 render 函数将 Vnode 渲染到页面
+    // 2. 使用 render 函数将 Vnode 渲染到页面
     // 参数一：Vnode    参数二：DOM 节点
     render(messageVnode, mountNode);
 
-    // 一个外部可访问的清除的方法
+    // 3. 一个外部可访问的清除的方法
     const destory = () => {
         render(null, mountNode)
         document.body.removeChild(mountNode);
     }
 
-    // 如果传入了显示多长时间
+    // 如果传入了显示时长
     if(timeout) {
         // 显示的时间到后清除组件和节点
         setTimeout(()=>{
